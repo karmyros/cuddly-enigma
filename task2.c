173b919ef6c06c10d3d66b72256a3c8a54709dc1
Myroslava <kart.mir06@gmail.com>
2024-07-05 20:22:36 +0300

    Практичне завдання №2(НСК): 1. написана програма знаходження НСК чисел кількістю p(2-20); 2. доповнена інформація у файлі README про виконану роботу.

---
+++ task2.c
@@ -0,0 +1,59 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int nsd(int a, int b) {
+    while (b > 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int nsk(int a, int b) {
+    return a * (b / nsd(a, b));
+}
+
+int nskArr (int nums[], int p) {
+    int result = nums[0];
+    for (int i = 1; i < p; i++) {
+        result = nsk(result, nums[i]);
+    }
+    return result;
+}
+
+int main () {
+    while (1 > 0) {
+        int p = 0, isNat = 1;
+        char tempChr;
+
+        printf("Введіть кількість чисел(2-20), для яких буде знайдено НСК: ");
+        scanf("%d", &p);
+
+        if (p < 2 || p > 20) {
+            printf("Кількість не входить у задані межі. Спробуйте ще раз. \n\n");
+            scanf("%c", tempChr);
+        } else {
+            int numbers[p];
+            int nsk = 1;
+
+            printf("Введіть ваші натуральні числа (%d шт, більших за 0): ", p);
+            for (int i = 0; i < p; i++) {
+                scanf("%d", &numbers[i]);
+                if (numbers[i] <= 0) {
+                    isNat = 0;
+                }
+            }
+
+            if (isNat == 0) {
+                printf("Деякі з отриманих чисел не натуральні. Спробуйте ще раз.\n\n");
+                scanf("%c", tempChr);
+            } else {
+                nsk = nskArr(numbers, p);
+                printf("НСК введених чисел дорівнює %d", nsk);
+                return 0;
+            }
+        }
+    }   
+}
